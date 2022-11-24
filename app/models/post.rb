d23f9ac7a4b53280ab7911dbac2672da3c385b86
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-24 10:41:25 +0100

    Update Post model and add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-  has_many :comments
-  has_many :likes
-  belongs_to :users
+  belongs_to :author, class_name: 'User'
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
+
+  after_save :update_post_counter
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.all.length)
+  end
 end
