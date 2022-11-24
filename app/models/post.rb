023becf6379208de73e765ff60a99c4154d6bb7d
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-24 22:54:39 +0100

    Add validation for Post model for title, comment_counter and likes_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
+
+  validates :title, presence: true length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
   
 
   after_save :update_post_counter
