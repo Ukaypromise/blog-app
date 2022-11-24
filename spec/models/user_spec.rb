e1b5d9c12f0f723614bde602bd2afb794cb4df61
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-24 23:54:54 +0100

    Setup spec unit test for User Model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'Promise', photo: 'https://unsplash.com/photos/M_-5YgbSdV3o', bio: 'I am a frontend developer', posts_counter: 4) }
+  before { subject.save }
+
+  it 'name should have attributes' do
+    subject.name = 'Promise'
+    expect(subject).to be_valid
+  end
+
+  it 'name should not be blank or less than 3 letters' do
+    subject.name = nil
+    subject.name = 'Pr'
+    expect(subject).to_not be_valid
+  end
+
+  it 'returns photo if valid' do
+    subject.photo = 'https://unsplash.com/photos/M_-5YgbSdV3o'
+    expect(subject).to be_valid
+  end
+
+  it 'is valid with a bio' do
+    subject.bio = 'I am a frontend developer'
+    expect(subject).to be_valid
+  end
+
+  it 'post_counter should must be an integer' do
+    subject.posts_counter = 1
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid if post counter is a string or nil' do
+    subject.posts_counter = 'four'
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+end
