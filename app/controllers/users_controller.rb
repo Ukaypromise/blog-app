33613846e5446697fb1cbc8e3b083ab465dd0f41
Ukaypromise <promiseukaimo@gmail.com>
2022-12-08 13:14:41 -0500

    Fix some errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.includes(:posts).find(params[:id])
+    # @user = User.includes(:posts).find(params[:id])
+    @user = User.find(params[:id])
   end
 end
